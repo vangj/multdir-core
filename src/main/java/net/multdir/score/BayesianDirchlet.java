561f194d32f15811b7361c2685bbd084ec04dc73
vangj <vangjee@gmail.com>
2014-03-22 20:26:28 -0400

    - add javadoc comments

--- src/main/java/net/multdir/score/BayesianDirchlet.java
+++ src/main/java/net/multdir/score/BayesianDirchlet.java
@@ -2,14 +2,26 @@ package net.multdir.score;
 
 import java.util.List;
 
+/**
+ * Bayesian dirichlet.
+ *
+ */
 public class BayesianDirchlet {
 
 	protected List<BetaFunc> betas;
 	
+	/**
+	 * Constructor.
+	 * @param builder {@link BayesianDirchletBuilder}.
+	 */
 	protected BayesianDirchlet(BayesianDirchletBuilder builder) {
 		this.betas = builder.getBetas();
 	}
 	
+	/**
+	 * Gets the score.
+	 * @return Score.
+	 */
 	public double get() {
 		if(null == betas || betas.size() < 1)
 			return Double.NEGATIVE_INFINITY;
